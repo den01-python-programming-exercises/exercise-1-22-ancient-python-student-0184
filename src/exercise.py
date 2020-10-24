5637a51fb00d3fe07bac2d1728e0a4de44fda269
pythonstudent01 <python-student@den01.co.uk>
2020-10-24 10:11:34 +0000

    Attempt solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,8 @@
 def main():
     #write your code below this line
+    year = int(input("Give a year:"))
+    if year < 2015:
+      print("Ancient history!")
 
 if __name__ == '__main__':
     main()
